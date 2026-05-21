dfbea8a8187e51e610bf44d8e4a98c27bf5e97b9
alfmartens <alfmchess@gmail.com>
2026-05-21 22:37:43 +0200

    Add alt texts for the images

--- scripts/main.js
+++ scripts/main.js
@@ -8,6 +8,7 @@ items.forEach(item => {
     item.addEventListener("click", () => {
         if (item.textContent == "Äppelviken") {
             myImage.setAttribute("src", "images/Äppelviken,_Snäckvägen_22,_2012.jpg");
+            myImage.setAttribute("alt", "View of Äppelviken");
             myHeading.textContent = "Äppelviken";
             myParagraph.textContent = "When you go by Nockebybanan from Alvik, you'll first arrive at Äppelviken. " +
                                       "Äppelviken was mostly built during the 1910s and 1920s, and was the earliest built of the five districts. " +
@@ -15,12 +16,14 @@ items.forEach(item => {
         }
         else if (item.textContent == "Smedslätten") {
             myImage.setAttribute("src", "images/Smedslatten_2008c.jpg");
-            myHeading.textContent = "Smedslätten";
+            myImage.setAttribute("alt", "View of Smedslätten");
+           myHeading.textContent = "Smedslätten";
             myParagraph.textContent = "After Äppelviken, the next stop when you go by Nockebybanan from Alvik is Smedslätten. " +
                                       "This district was built mostly during the 1920s.";
         }
         else if (item.textContent == "Ålsten") {
             myImage.setAttribute("src", "images/Alsten_4.jpg");
+            myImage.setAttribute("alt", "View of Ålsten");
             myHeading.textContent = "Ålsten";
             myParagraph.textContent = "West of Smedslätten is Ålsten, famous for its townhouses, the so called Per-Albin houses, " +
                                       "named after a former Swedish prime minister that lived in one of these. They were built from 1932 to 1933. " +
@@ -28,12 +31,14 @@ items.forEach(item => {
         }
         else if (item.textContent == "Höglandet") {
             myImage.setAttribute("src", "images/Hoglandet_1.jpg");
+            myImage.setAttribute("alt", "View of Höglandet");
             myHeading.textContent = "Höglandet";
             myParagraph.textContent = "The next stop after Ålsten is Höglandet. This district was mostly built during the 1920s and 1930s. " +
                                       "It is the smallest of the five districts, both in terms of population and area.";
         }
         else if (item.textContent == "Nockeby") {
             myImage.setAttribute("src", "images/Nockeby_2010b.jpg");
+            myImage.setAttribute("alt", "View of Nockeby");
             myHeading.textContent = "Nockeby";
             myParagraph.textContent = "The last stop of Nockebybanan when you travel from Alvik is Nockeby itself. " +
                                       "This district was mostly built during the 1930s.";
